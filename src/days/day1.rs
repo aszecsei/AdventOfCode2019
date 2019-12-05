1c3f8357239238b2cd0b0143fe4c7276d820a861
Alic Szecsei <alszecse@microsoft.com>
2019-12-05 12:11:19 -0800

    Refactor day 1 to use successor fn

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -11,30 +11,23 @@ pub fn input_generator_day1(input: &str) -> Vec<i64> {
 
 #[inline]
 fn fuel_cost(mass: i64) -> i64 {
-    (mass / 3) - 2
+    std::cmp::max(0, (mass / 3) - 2)
 }
 
 #[aoc(day1, part1)]
 pub fn solve_day1_part1(input: &[i64]) -> i64 {
-    input.iter().map(|x| fuel_cost(*x)).sum()
+    input.iter().map(|&x| fuel_cost(x)).sum()
 }
 
 #[aoc(day1, part2)]
 pub fn solve_day1_part2(input: &[i64]) -> i64 {
     input
         .iter()
-        .map(|x| {
-            let mut fuel = fuel_cost(*x);
-            let mut just_added = fuel;
-            loop {
-                let new_req = fuel_cost(just_added);
-                if new_req <= 0 {
-                    break;
-                }
-                fuel += new_req;
-                just_added = new_req;
-            }
-            fuel
+        .map(|&x| fuel_cost(x))
+        .flat_map(|x| {
+            std::iter::successors(Some(x), |&x| {
+                if x == 0 { None } else { Some(fuel_cost(x)) }
+            })
         })
         .sum()
 }
